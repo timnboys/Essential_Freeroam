e17b8ddc54328c601257343bbd7a06aea853948d
Glenn Hermans <glhermans@gmail.com>
2017-03-02 10:10:45 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,57 @@
+local permission = {
+	kick = 1,
+	ban = 4
+}
+
 RegisterServerEvent('playerSpawn')
 AddEventHandler('playerSpawn', function()
-	-- Spawn the player at: X: 100, Y: 100, Z: 100
-	TriggerClientEvent('es_freeroam_gamemode:spawnPlayer', source, 100.0, 100.0, 100.0 --[[, model -- you can put a model here]])
+	-- Spawn the player at: X:436.491 Y: -982.172 Z:30.699
+	TriggerClientEvent('es_freeroam:spawnPlayer', source, 436.491, -982.172, 30.699)
+end)
+
+
+-- Money Management
+AddEventHandler('es:playerLoaded', function(source)
+TriggerEvent('es:getPlayerFromId', source, function(user)
+
+-- Activate the money for the current player
+TriggerClientEvent('es:activateMoney', source, tonumber(user.money))
+
+-- Send the player some information regarding the money
+TriggerClientEvent('chatMessage', source, "SYSTEM", {187, 235, 42}, "Your money amount is: $" .. tonumber(user.money))
+
+	end)
+end)
+
+
+-- Kicking
+TriggerEvent('es:addAdminCommand', 'kick', permission.kick, function(source, args, user)
+		if(GetPlayerName(tonumber(args[2])))then
+			local player = tonumber(args[2])
+
+			-- User permission check
+			TriggerEvent("es:getPlayerFromId", player, function(target)
+				if(tonumber(target.permission_level) > tonumber(user.permission_level))then
+					TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You're not allowed to target this person!")
+					return
+				end
+
+				local reason = args
+				table.remove(reason, 1)
+				table.remove(reason, 1)
+				if(#reason == 0)then
+					reason = "Kicked: You have been kicked from the server"
+				else
+					reason = "Kicked: " .. table.concat(reason, " ")
+				end
+
+				TriggerClientEvent('chatMessage', -1, "SYSTEM", {255, 0, 0}, "Player ^2" .. GetPlayerName(player) .. "^0 has been kicked(^2" .. reason .. "^0)")
+				DropPlayer(player, reason)
+			end)
+		else
+			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Incorrect player ID!")
+		end
+ end,
+   function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
 end)
