3d37949d98435b5e0df176538d6ddd40a0aad713
Glenn Hermans <glhermans@gmail.com>
2017-03-02 10:12:54 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,6 @@
-resource_type 'gametype' { name = 'es_freeroam_gamemode' }
+resource_type 'gametype' { name = 'es_freeroam'}
+
+description 'FiveM Essential FreeRoam.'
 
 -- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
