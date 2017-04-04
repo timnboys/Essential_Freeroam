29afeaa49401da3e3263914bedf5b01c858bd2a5
Glenn Hermans <glhermans@gmail.com>
2017-04-04 05:26:31 +0200

    Added option to buy drinks at the stripclub

--- stores/stripclub.lua
+++ stores/stripclub.lua
@@ -22,6 +22,11 @@ function LocalPed()
   return GetPlayerPed(-1)
 end
 
+function StartText()
+  DrawMarker(1, -1171.42, -1572.72, 3.6636, 0, 0, 0, 0, 0, 0, 4.0, 4.0, 2.0, 178, 236, 93, 155, 0, 0, 2, 0, 0, 0, 0)
+  ShowInfo("Press ~INPUT_CONTEXT~ to buy a drink", 0)
+end
+
 Citizen.CreateThread(function()
   -- Load the ped modal (s_f_y_bartender_01)
   RequestModel(GetHashKey("s_f_y_bartender_01"))
@@ -88,5 +93,64 @@ Citizen.CreateThread(function()
     SetPedRelationshipGroupHash(stripper, GetHashKey("army"))
     TaskPlayAnim(stripper,"mini@strip_club@idles@stripper","stripper_idle_03", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
   end
-
 end)
+
+
+local playerCoords
+local playerPed
+showStartText = false
+
+Citizen.CreateThread(function()
+    while true do
+       Wait(0)
+       playerPed = GetPlayerPed(-1)
+       playerCoords = GetEntityCoords(playerPed, 0)
+
+       if(GetDistanceBetweenCoords(playerCoords, 128.900, -1283.21, 29.273) < 2) then
+         if(showStartText == false) then
+           StartText()
+         end
+
+         -- Start mission
+         if(IsControlPressed(1, 38)) then
+           TriggerServerEvent("es_freeroam:pay", tonumber(50))
+           Toxicated()
+           Citizen.Wait(120000)
+           reality()
+         end
+       else
+         showStartText = false
+       end --if GetDistanceBetweenCoords ...
+    end
+end)
+
+
+function Toxicated()
+  RequestAnimSet("move_m@drunk@verydrunk")
+  while not HasAnimSetLoaded("move_m@drunk@verydrunk") do
+    Citizen.Wait(0)
+  end
+
+  TaskStartScenarioInPlace(GetPlayerPed(-1), "WORLD_HUMAN_DRUG_DEALER", 0, 1)
+  DoScreenFadeOut(1000)
+  Citizen.Wait(1000)
+  ClearPedTasksImmediately(GetPlayerPed(-1))
+  SetTimecycleModifier("spectator5")
+  SetPedMotionBlur(GetPlayerPed(-1), true)
+  SetPedMovementClipset(GetPlayerPed(-1), "move_m@drunk@verydrunk", true)
+  SetPedIsDrunk(GetPlayerPed(-1), true)
+  DoScreenFadeIn(1000)
+  end
+
+  function reality()
+    DoScreenFadeOut(1000)
+    Citizen.Wait(1000)
+    DoScreenFadeIn(1000)
+    ClearTimecycleModifier()
+    ResetScenarioTypesEnabled()
+    ResetPedMovementClipset(GetPlayerPed(-1), 0)
+    SetPedIsDrunk(GetPlayerPed(-1), false)
+    SetPedMotionBlur(GetPlayerPed(-1), false)
+    -- Stop the toxication
+    Citizen.Trace("Going back to reality\n")
+    end
